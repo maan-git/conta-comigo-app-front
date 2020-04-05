8bdb44c3f0e9058b450cbad18c5edffb8f911750
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-04-04 21:42:04 -0300

    Validação da configuração da applicação.

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 5000;
+const serveStatic = require('serve-static');
+
+const app = express();
+//app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname +"/dist/index.html")
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port);
+
+console.log('server started '+ port);
