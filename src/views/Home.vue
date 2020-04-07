7952ed4772814b05acf10e1d910f8463c8d969fb
abrantesmar <abs.marcio@gmail.com>
2020-04-07 17:14:07 -0300

    component button with router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,20 @@
     <ListHelp/>
     <v-btn text block color="primary">ver todos</v-btn>
     <h3 class="primary--text">Voluntarios</h3>
-    <v-btn
-      rounded
-      block
-      color="primary"
-      x-large
-    >
-      Preciso de ajuda!
-      <v-icon>$heart</v-icon>
-    </v-btn>
+    <router-link
+        to="/CreateHelp"
+        tag="v-btn"
+      >
+      <v-btn
+        rounded
+        block
+        color="primary"
+        x-large
+      >
+        Preciso de ajuda!
+        <v-icon>$heart</v-icon>
+      </v-btn>
+    </router-link>
   </v-container>
 </template>
 
