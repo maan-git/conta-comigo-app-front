7bb73a2d614d0bdc37b9a59ac92d2f86ed9bff1f
Everton Melo <evertonsquaq@gmail.com>
2020-04-13 23:37:02 +0100

    side nav and navbar layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app :class="purple()">
     <!--  -->
-    <NavBar v-on:toogle="toogleMenu()"/>
     <SideMenu ref="sidemenu"/>
-    <v-content>
+    <NavBar v-on:toogle="toogleMenu()"/>
+    <v-content class="content-width">
       <router-view></router-view>
     </v-content>
   </v-app>
@@ -42,5 +42,10 @@ export default {
 .theme--light.purple {
   background-color: #532594;
   background-image: url('./assets/pattern.svg');
+  transition: all .3s ease-in-out;
+}
+.content-width {
+  margin: 0 auto;
+  max-width: 1200px;
 }
 </style>
