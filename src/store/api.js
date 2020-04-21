9fe37ce371eda8866dda4914179d8a3455b946aa
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-04-21 17:09:55 -0300

    Adjusting the SSL certification on heroku and trying it on the app.

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const apiUrl = 'https://reqres.in/api/';
 // const apiUrl = 'https://conta-comigo-ap.herokuapp.com/';
-const apiUrl = 'http://www.helpmecorona.com/';
+const apiUrl = 'https://www.helpmecorona.com/';
 
 export default () => axios.create({
   baseURL: apiUrl,
