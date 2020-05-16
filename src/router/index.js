63dc113671f82c844093fdda8e415de92f51231b
Everton Melo <evertonsquaq@gmail.com>
2020-05-16 20:19:58 +0100

    icons

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const routes = [
     path: '/about',
     name: 'About',
     component: () => import('../views/About.vue'),
-    beforeEnter: guard,
+    // beforeEnter: guard,
   },
   {
     path: '/create-help',
