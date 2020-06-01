309152a37b2582175f6ccd6e1773cffa96c1d5b5
Everton Melo <evertonsquaq@gmail.com>
2020-06-01 20:28:49 +0100

    page not found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="notfound">
+    <h1 class="white--text">
+      404 Página não encontrada <span class=" danger--text">:/</span></h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .notfound {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
